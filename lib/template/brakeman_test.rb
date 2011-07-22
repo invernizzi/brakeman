ad75b71e8a73c663f1613c23e83d962a30effe1e
Luca Invernizzi <invernizzi.l@gmail.com>
2011-07-22 16:13:09 -0700

    Template test case added

---
+++ lib/template/brakeman_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+require 'brakeman'
+
+class BrakemanTest < ActionController::IntegrationTest
+
+  def test_static_analyisis
+    options = {:app_path => ::Rails.root.to_s,
+            :output_file => File.join(::Rails.root.to_s, "tmp/brakeman/index.html")}
+    #Make sure that no issue are found
+    assert_true Brakeman.main(options) == 0
+  end
+end
